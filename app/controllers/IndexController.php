ec740cfa57c22bc6a5f6e9f1e480ddd2a9d46204
ROULLAND <73873954+Thomas-roulland@users.noreply.github.com>
2022-02-08 12:12:29 +0100

    création de function pour afficher avec twig les éléments de la bdd, + revu du fonctionnement de twig. Prochaine étape afficher et automatiser avec twig

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,8 +2,13 @@
 namespace controllers;
 
 use Ajax\JsUtils;
+use models\Groupe;
+use models\Route;
+use models\Serveur;
+use models\User_;
 use Ubiquity\core\postinstall\Display;
 use Ubiquity\log\Logger;
+use Ubiquity\orm\DAO;
 use Ubiquity\themes\ThemesManager;
 
 /**
@@ -16,4 +21,29 @@ class IndexController extends ControllerBase {
         $this->jquery->renderView('Index/index.html');
 	}
 
+    public function listVMUser() {
+        $user_id = USession::get('user_id');
+        $vm = DAO::getAll(Vm::class, 'idUser = :idUser', false, ['idUser' => $user_id]);
+        $this->jquery->renderView('Index/index.html', ['vms' => $vm]);
+    }
+
+    public function listServeurAll() {
+        $server = DAO::getAll(Serveur::class);
+        $this->jquery->renderView('Index/index.html', ['servers' => $server]);
+    }
+
+    public function listUser(){
+        $user = DAO::getAll(User_::class);
+        $this->jquery->renderView('Index/index.html', ['users' => $user]);
+    }
+
+    public function listGroup(){
+        $groupAll = DAO::getAll(Groupe::class);
+        $this->jquery->renderView('Index/index.html', ['groups' => $groupAll]);
+    }
+
+    public function listRoute(){
+        $route = DAO::getAll(Route::class);
+        $this->jquery->renderView('Index/index.html', ['routes' => $route]);
+    }
 }
