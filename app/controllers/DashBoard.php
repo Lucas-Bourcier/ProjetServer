3a10d9996121c0f64021aca7504771e93548717b
ROULLAND <73873954+Thomas-roulland@users.noreply.github.com>
2022-02-14 16:42:45 +0100

    header&footer pour dashboard différent

--- app/controllers/DashBoard.php
+++ app/controllers/DashBoard.php
@@ -1,9 +1,27 @@
 <?php
 namespace controllers;
+ use Ubiquity\utils\http\URequest;
+
  /**
   * Controller DashBoard
   */
 class DashBoard extends \controllers\ControllerBase{
+
+    protected $headerView = "@activeTheme/main/vHeaderDashBoard.html";
+
+    protected $footerView = "@activeTheme/main/vFooterDashBoard.html";
+
+    public function initialize() {
+        if (! URequest::isAjax()) {
+            $this->loadView($this->headerView);
+        }
+    }
+
+    public function finalize() {
+        if (! URequest::isAjax()) {
+            $this->loadView($this->footerView);
+        }
+    }
 
 	public function index(){
 		$this->loadView("DashBoard/index.html");
