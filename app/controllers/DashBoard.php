68ac448540357e0496ef666ee24f41078f77d11f
ROULLAND <73873954+Thomas-roulland@users.noreply.github.com>
2022-02-15 17:11:58 +0100

    dashboard accueil terminé, voir ses vm aussi, profil en cour ainsi que option

--- app/controllers/DashBoard.php
+++ app/controllers/DashBoard.php
@@ -1,5 +1,9 @@
 <?php
 namespace controllers;
+ use models\User_;
+ use models\Vm;
+ use Ubiquity\attributes\items\router\Route;
+ use Ubiquity\orm\DAO;
  use Ubiquity\utils\http\URequest;
  use Ubiquity\utils\http\USession;
 
@@ -27,6 +31,20 @@ class DashBoard extends \controllers\ControllerBase{
 	public function index(){
 		$this->loadView("DashBoard/index.html", ['name' =>USession::get('name')]);
 	}
+
+    #[Route('/DashBoard/VM', name: 'dash.vm')]
+    public function DashVm(){
+        $user_id = USession::get('user_id');
+        $vm = DAO::getAll(Vm::class, 'idUser = :idUser', false, ['idUser' => $user_id]);
+        $this->loadView("DashBoard/DashVm.html", ['vms' => $vm]);
+    }
+
+    #[Route('/DashBoard/Profil', name: 'dash.profil')]
+    public function DashProfil(){
+        $user_id = USession::get('user_id');
+        $user = DAO::getById(User_::class, $user_id);
+        $this->loadView("DashBoard/Dashprofil.html", ['users' => $user]);
+    }
 
 
 }
